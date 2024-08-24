321507c76daede7bce5942682e40890876c1275f
OscarCastellanos01 <oscareliascastellanoscordero@gmail.com>
2024-08-23 18:58:05 -0600

    Mejoras en las funciones de conversion

--- app/Http/Controllers/ConversionController.php
+++ app/Http/Controllers/ConversionController.php
@@ -9,14 +9,15 @@ class ConversionController extends Controller
     public function textToBinary($text) {
         $binary = '';
         for ($i = 0; $i < strlen($text); $i++) {
-            $binary .= sprintf("%08b", ord($text[$i]));
+            $binary .= sprintf("%08b", ord($text[$i])) . ' ';
         }
-        return $binary;
+        return trim($binary); // Elimina el espacio final extra
     }
 
     public function binaryToText($binary) {
         $text = '';
-        $binaryArray = str_split($binary, 8);
+        // Elimina espacios adicionales y divide en grupos de 8 bits
+        $binaryArray = str_split(str_replace(' ', '', $binary), 8);
         foreach ($binaryArray as $bin) {
             $text .= chr(bindec($bin));
         }
